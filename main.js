3ea9386509c5fb5abf23e090812fb0d43e5c52f3
sarah niemiec <sroseniemiec@gmail.com>
2017-03-28 12:21:46 -0400

    added song info

--- main.js
+++ main.js
@@ -1,41 +1,68 @@
+SC.initialize({client_id: 'fd4e76fc67798bfa742089ed619084a6'})
+
+
 var playButton = document.querySelector("#play")
 var pauseButton = document.querySelector("#pause")
 var stopButton = document.querySelector("#stop")
-// var nextButton = document.querySelector("#next")
-// var backButton = document.querySelector("#back")
-// var newSongButton = document.querySelector("#newsong")
+var nextButton = document.querySelector("#next")
+var backButton = document.querySelector("#back")
+var newSongButton = document.querySelector("#newsong")
+
+var titleSpot = document.querySelector("#titlespot")
+var titleLinkSpot = document.querySelector("#titlelinkspot")
+var artistSpot = document.querySelector("#artistspot")
+var artistLinkSpot = document.querySelector("#artistlinkspot")
+var descriptionSpot = document.querySelector("#descriptionspot")
+var genreSpot = document.querySelector("#genrespot")
+var dateSpot = document.querySelector("#datespot")
+var artworkSpot = document.querySelector("#artworkspot")
 
 var index = [0]
-var titleSpot = document.querySelector("#titlespot")
-var artistSpot = document.querySelector("#artistspot")
-
 var songs = []
 
-var titles = "Multi-Love https://soundcloud.com/jagjaguwar/unknown-mortal-orchestra-multi-love"
-
-var artists = "Unknown Mortal Orchestra"
-
-var descriptions = "Unknown Mortal Orchestra - “Multi-Love” from THE ALBUM ‘Multi-Love’ out May 26th, 2015 on Jagjaguwar↵↵SCD/direct: http://smarturl.it/umo?iQid=sc // Amazon: http://smarturl.it/umo_amazon?iQid=sc // iTunes: http://smarturl.it/umo_itunes?iQid=sc // Indies: http://smarturl.it/umo_indies?iQid=sc↵↵Unknown Mortal Orchestra↵Artist Page http://www.jagjaguwar.com/artist.php?name=umo↵Website http://unknownmortalorchestra.com/↵Facebook https://www.facebook.com/unknownmortalorchestra↵Twitter https://twitter.com/UMO↵Instagram http://instagram.com/unknownmortalorchestra↵↵Unknown Mortal Orchestra - “Multi-Love” from ‘Multi-Love’ out May 26th, 2015 on Jagjaguwar genre: Indie"
-
-var trackId = 189137400
+var trackId = [287747010]
+var cloudsong = SC.stream('/tracks/' + trackId[index])
 
 
 
-
-SC.initialize({client_id: 'fd4e76fc67798bfa742089ed619084a6'})
-
-SC.resolve("https://soundcloud.com/jagjaguwar/unknown-mortal-orchestra-multi-love").then(function(response) {
+SC.resolve("https://soundcloud.com/bodylanguage/social-studies").then(function(response) {
   console.log(response)
 })
 
 
-var cloudsong = SC.stream('/tracks/' + trackId)
-console.log(cloudsong)
-
-
-
 function Jukebox(songs) {
   this.songs = songs
+
+  SC.resolve("https://soundcloud.com/bodylanguage/social-studies").then(function(response) {
+    var artwork = response.artwork_url
+    var img = document.createElement('img')
+    img.src = artwork
+    artworkSpot.appendChild(img)
+    var title = response.title
+    titleSpot.innerHTML = "Title: " + title
+    var titlelink = response.permalink_url
+    var a = document.createElement('a')
+    a.setAttribute('href', titlelink)
+    a.setAttribute('target', '_blank')
+    a.innerHTML = "link to track"
+    titleLinkSpot.appendChild(a)
+    var artist = response.user.username
+    artistSpot.innerHTML = "Artist: " + artist
+    var artistlink = response.user.permalink_url
+    var anchor = document.createElement('a')
+    anchor.setAttribute('href', artistlink)
+    anchor.setAttribute('target', '_blank')
+    anchor.innerHTML = "link to artist page"
+    artistLinkSpot.appendChild(anchor)
+    var description = response.description
+    descriptionSpot.innerHTML = "Description: " + description
+    var genre = response.genre
+    genreSpot.innerHTML = "Genre: " + genre
+    var month = response.release_month
+    var day = response.release_day
+    var year = response.release_year
+    dateSpot.innerHTML = "Release Date: " + month + "/" + day + "/" + year
+  })
 }
 
 Jukebox.prototype.play = function () {
@@ -44,40 +71,12 @@ Jukebox.prototype.play = function () {
   })
 }
 
-
 Jukebox.prototype.pause = function () {
     cloudsong.then(function(player){
     player.pause()
   })
 }
 
-Jukebox.prototype.stop = function () {
-    cloudsong.then(function(player){
-    player.pause()
-    cloudsong.currentTime = 0
-  })
-}
-
-// Jukebox.prototype.forward = function () {
-//   this.songs[index].pause()
-//   this.songs[index].currentTime = 0
-//   index ++
-//   this.songs[index]
-//   if (index === this.songs.length) {
-//       index = [0]
-//   }
-//   this.songs[index].play()
-// }
-//
-// Jukebox.prototype.back = function () {
-//   this.songs[index].pause()
-//   this.songs[index].currentTime = 0
-//   index --
-//   if (index < 0) {
-//       index = this.songs.length -1
-//   }
-//   this.songs[index].play()
-// }
 
 
 var jukebox = new Jukebox(songs)
@@ -85,59 +84,14 @@ var jukebox = new Jukebox(songs)
 
 playButton.addEventListener("click", function(event){
   event.preventDefault()
-  titleSpot.innerHTML = titles
-  artistSpot.innerHTML = artists
   jukebox.play()
   playButton.style.color = "#ff4f5a"
   pauseButton.style.color = "#2d2d2d"
-  stopButton.style.color = "#2d2d2d"
 })
 
-pauseButton.addEventListener("click", function(event){
+pauseButton.addEventListener("click", function(event) {
   event.preventDefault()
   jukebox.pause()
   pauseButton.style.color = "#ff4f5a"
   playButton.style.color = "#2d2d2d"
-  stopButton.style.color = "#2d2d2d"
 })
-
-stopButton.addEventListener("click", function(event) {
-  event.preventDefault()
-  jukebox.stop()
-  stopButton.style.color = "#ff4f5a"
-  playButton.style.color = "#2d2d2d"
-  pauseButton.style.color = "#2d2d2d"
-})
-
-// nextButton.addEventListener("click", function(event){
-//   event.preventDefault()
-//   jukebox.forward()
-//   titleSpot.innerHTML = titles[index]
-//   artistSpot.innerHTML = artists[index]
-//   playButton.style.color = "#ff4f5a"
-//   pauseButton.style.color = "#2d2d2d"
-//   stopButton.style.color = "#2d2d2d"
-// })
-//
-// backButton.addEventListener("click", function(event){
-//   event.preventDefault()
-//   jukebox.back()
-//   titleSpot.innerHTML = titles[index]
-//   artistSpot.innerHTML = artists[index]
-//   playButton.style.color = "#ff4f5a"
-//   pauseButton.style.color = "#2d2d2d"
-//   stopButton.style.color = "#2d2d2d"
-// })
-
-// newSongButton.addEventListener("click", function(event) {
-//   event.preventDefault()
-//   var songField = document.querySelector("#songfield")
-//   var newSong = new Audio(songField.value)
-//   var titleField = document.querySelector("#titlefield")
-//   var newTitle = "Title: " + titleField.value
-//   var artistField = document.querySelector("#artistfield")
-//   var newArtist = "Artist: " + artistField.value
-//   songs.push(newSong)
-//   titles.push(newTitle)
-//   artists.push(newArtist)
-// })
