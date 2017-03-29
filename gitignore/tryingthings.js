40318854dbfd387bd5a300f507c09a8739054829
sarah niemiec <sroseniemiec@gmail.com>
2017-03-28 20:57:59 -0400

    finished assignment

--- gitignore/tryingthings.js
+++ gitignore/tryingthings.js
@@ -5,6 +5,7 @@ var playButton = document.querySelector("#play")
 var pauseButton = document.querySelector("#pause")
 var nextButton = document.querySelector("#next")
 var backButton = document.querySelector("#back")
+var newSongButton = document.querySelector("#newsong")
 
 var titleSpot = document.querySelector("#titlespot")
 var titleLinkSpot = document.querySelector("#titlelinkspot")
@@ -17,18 +18,33 @@ var artworkSpot = document.querySelector("#artworkspot")
 
 var index = [0]
 var songs = []
+var currentSong = [0]
 
-var trackId = [287747010]
+function Track (){
+  this.trackId = []
+}
+var trackId = [287747010, 280749864]
 var cloudsong = SC.stream('/tracks/' + trackId[index])
+
+SC.resolve("https://soundcloud.com/daptone-records/the-olympians-sirens-of-jupiter").then(function(response){
+  console.log(response)
+})
 
 
 SC.resolve("https://soundcloud.com/bodylanguage/social-studies").then(function(response) {
   console.log(response)
 })
 
+// var search = document.querySelector("#search")
 
-function Jukebox(songs) {
-  this.songs = songs
+// SC.get("/tracks", {q: search.innerHTML }).then(function(response){
+// console.log(response)
+// var newTrack = response[0].id
+// newTrack.push
+// });
+
+function Jukebox(trackId) {
+  this.trackId = trackId
 
   SC.resolve("https://soundcloud.com/bodylanguage/social-studies").then(function(response) {
     var artwork = response.artwork_url
@@ -65,13 +81,7 @@ function Jukebox(songs) {
 Jukebox.prototype.play = function () {
     cloudsong.then(function(player){
     player.play()
-    player.on("finish", function(){
-      player.pause()
-      cloudsong.currentTime = 0
-      pauseButton.style.color = "#ff4f5a"
-      playButton.style.color = "#2d2d2d"
-      player.play()
-      })
+
   })
 }
 
@@ -82,8 +92,17 @@ Jukebox.prototype.pause = function () {
 }
 
 
-var jukebox = new Jukebox(songs)
 
+
+
+Jukebox.prototype.forward = function playNext(){
+  SC.stream("/tracks/" + songs[currentSong].id).then(function(player) {player.play();
+      player.on("finish",function(){ currentSong += 1;
+            playNext()     })   }) }
+  player.play()
+}
+
+var jukebox = new Jukebox(trackId)
 
 playButton.addEventListener("click", function(event){
   event.preventDefault()
@@ -97,4 +116,14 @@ pauseButton.addEventListener("click", function(event) {
   jukebox.pause()
   pauseButton.style.color = "#ff4f5a"
   playButton.style.color = "#2d2d2d"
+})
+
+nextButton.addEventListener("click", function(event){
+  event.preventDefault()
+  jukebox.forward()
+  titleSpot.innerHTML = titles[index]
+  artistSpot.innerHTML = artists[index]
+  playButton.style.color = "#ff4f5a"
+  pauseButton.style.color = "#2d2d2d"
+  stopButton.style.color = "#2d2d2d"
 })
